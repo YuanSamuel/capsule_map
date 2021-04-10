10cc0f36dbecaadd12b3d3fa76377867db3d7ab2
YuanSamuel <samuel6161@gmail.com>
2021-04-10 18:25:45 -0500

    :building_construction: add position stream and Firebase storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,33 @@
+import 'package:capsule_map/screens/root_screen.dart';
 import 'package:capsule_map/screens/signup_screen.dart';
-import 'package:capsule_map/screens/welcome_screen.dart';
+import 'package:capsule_map/stores/mainStore/main_store.dart';
+import 'package:capsule_map/stores/positionStore/position_store.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
+    return MultiProvider(
+      providers: [
+        Provider<MainStore>(
+          create: (_) => MainStore()..setupReactions(),
+        ),
+        Provider<PositionStore>(
+            create: (_) => PositionStore()..checkPermissions()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: RootScreen(),
+      ),
     );
   }
 }
