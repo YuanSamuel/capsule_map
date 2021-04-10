2f923afb508649c8972d9743ac66556d1a08f72b
lisaliuu <lisaliuu2016@gmail.com>
2021-04-10 14:58:06 -0500

    login with messed up circles

---
+++ lib/screens/friendrequest_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+class FriendRequestScreen extends StatefulWidget {
+  @override
+  _FriendRequestScreenState createState() => _FriendRequestScreenState();
+}
+
+class _FriendRequestScreenState extends State<FriendRequestScreen> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery
+        .of(context)
+        .size
+        .height;
+    double width = MediaQuery
+        .of(context)
+        .size
+        .width;
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          iconTheme: IconThemeData(
+              color: Colors.white
+          ),
+          title: const Text('Saved Suggestions', style: TextStyle(
+              color: Colors.white
+          )),
+            leading: IconButton(
+              icon: Icon(
+                Icons.arrow_back_ios_rounded,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                // do something
+              },
+            ),
+          title: Text('Friend Request'),
+          centerTitle:true,
+
+
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: 100.0, vertical: 8.0),
+            child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment:
+                    MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment:
+                        CrossAxisAlignment.center,
+
+                        children: [
+                          Text(
+                            'Friend Request',
+                            style: TextStyle(
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xFF616161),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        color: Color(0xFF757575),
+                        size: 25.0,
+                      ),
+                    ],
+                  ),
+
+                ]
+            ),
+          ),
+        )
+
+    );
+  }
+}
