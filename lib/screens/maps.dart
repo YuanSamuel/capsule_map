1bc14a68e04b797287b6db07de8caee550723498
YuanSamuel <samuel6161@gmail.com>
2021-04-10 20:36:04 -0500

    :bug: remove bug

--- lib/screens/maps.dart
+++ lib/screens/maps.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-
-
-
-
 class MapCapsule extends StatefulWidget {
   @override
   _MapCapsuleState createState() => _MapCapsuleState();
@@ -14,9 +10,8 @@ class MapCapsule extends StatefulWidget {
 class _MapCapsuleState extends State<MapCapsule> {
   get markers => null;
 
-
 // ignore: unused_element
-void _updatePosition(CameraPosition _position) {
+  void _updatePosition(CameraPosition _position) {
     Position newMarkerPosition = Position(
         latitude: _position.target.latitude,
         longitude: _position.target.longitude);
@@ -24,11 +19,11 @@ void _updatePosition(CameraPosition _position) {
 
     setState(() {
       markers["1"] = marker.copyWith(
-          positionParam: LatLng(newMarkerPosition.latitude, newMarkerPosition.longitude));
+          positionParam:
+              LatLng(newMarkerPosition.latitude, newMarkerPosition.longitude));
     });
   }
-  
-  
+
   @override
   Widget build(BuildContext context) {
     var value;
@@ -36,54 +31,24 @@ void _updatePosition(CameraPosition _position) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Capsule Map'),
-
       ),
       body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(37.71124, -121.87365),
-          zoom: 7
-          ),
-          markers: Set<Marker>.of(
-            <Marker>[
-              Marker(
+          mapType: MapType.hybrid,
+          initialCameraPosition:
+              CameraPosition(target: LatLng(37.71124, -121.87365), zoom: 7),
+          markers: Set<Marker>.of(<Marker>[
+            Marker(
                 onTap: () {
-              print('Tapped');
-          },
-              draggable: true,
-              markerId: MarkerId('Marker'),
-              position: LatLng(value.latitude, value.longitude),
-              onDragEnd: ((newPosition) {
-                print(newPosition.latitude);
-                print(newPosition.longitude);
-          }))
-        
-        ]
-      )
-    
-    ),
-      
-
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
- 
-
-      
-  );
-
-
+                  print('Tapped');
+                },
+                draggable: true,
+                markerId: MarkerId('Marker'),
+                position: LatLng(value.latitude, value.longitude),
+                onDragEnd: ((newPosition) {
+                  print(newPosition.latitude);
+                  print(newPosition.longitude);
+                }))
+          ])),
+    );
   }
 }
