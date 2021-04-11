a0c1c62ed140a24933272a28fddd86c7db12ace3
YuanSamuel <samuel6161@gmail.com>
2021-04-10 20:33:48 -0500

    remove error

--- lib/stores/mainStore/main_store.g.dart
+++ lib/stores/mainStore/main_store.g.dart
@@ -9,7 +9,7 @@ part of 'main_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$MainStore on _MainStore, Store {
-  Computed<bool>? _$loggedInComputed;
+  Computed<bool> _$loggedInComputed;
 
   @override
   bool get loggedIn => (_$loggedInComputed ??=
