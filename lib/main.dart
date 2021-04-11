ba2e0b3a32fab4ffb0e082c99fe64e5c01b3f454
lisaliuu <lisaliuu2016@gmail.com>
2021-04-10 21:49:02 -0500

    ui capsiles

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: SignInScreen(),
+        home: RootScreen(),
       ),
     );
   }
