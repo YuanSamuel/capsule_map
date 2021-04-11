8594aa20ad61d97f22176005d230b70ec979c8bd
YuanSamuel <samuel6161@gmail.com>
2021-04-10 20:48:18 -0500

    :bug: fix up maps

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,21 +1,43 @@
+import 'dart:async';
+
 import 'package:capsule_map/stores/positionStore/position_store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
     PositionStore positionStore = Provider.of<PositionStore>(context);
 
     return Observer(builder: (_) {
-      return Scaffold(
-        body: Center(
-          child: Text(positionStore.positionStream != null
-              ? positionStore.positionStream.value.toString()
-              : 'Permission Disabled'),
-        ),
-      );
+      return positionStore.positionStream != null &&
+              positionStore.positionStream.value != null
+          ? Scaffold(
+              body: GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(
+                  positionStore.positionStream.value.latitude,
+                  positionStore.positionStream.value.longitude,
+                ),
+                zoom: 12.0,
+              ),
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ))
+          : Center(
+              child: CircularProgressIndicator(),
+            );
     });
   }
 }
